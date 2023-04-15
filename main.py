4e79274d8cc82c0d2643c8e3e13858d259aeacf4
Paul <paulbiard0@gmail.com>
2023-04-15 21:08:07 +0200

    Oublie de rendre convertir les chaine de caract du json en type cohérant

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import json
 
 if __name__ == '__main__':
 
+
     dataFrameWeather = extractData.extractDataToDataFrame('data/weather.json')
+    dataFrameWeather['temp'] = dataFrameWeather['temp'].astype('float')
+    dataFrameWeather['humidity'] = dataFrameWeather['humidity'].astype('float')
+    dataFrameWeather['pressure'] = dataFrameWeather['pressure'].astype('int')
+    dataFrameWeather['report_date'] = pandas.to_datetime(dataFrameWeather['report_date'])
     print(dataFrameWeather.head())
 
 
